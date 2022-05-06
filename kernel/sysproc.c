aadb9228739fe028b85ffd916618775209f9d205
tyf <1552068173@qq.com>
2022-05-06 17:54:57 +0800

    finish syscall trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,24 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_trace(void) {
+  int mask;
+
+  if(argint(0, &mask) < 0) {
+    return -1;
+  }
+
+  char* p = myproc()->mask;
+
+  for(int i = 0; i<23; ++i) {
+    if(mask % 2) {
+      p[i] = '1';
+    } else {
+      p[i] = '0';
+    }
+    mask >>= 1;
+  }
+
+  return 0;
+}
